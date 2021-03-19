12e8b05fcd3e614fd2fd77847c6df6205f464696
squarepancakes <goh.shuyun@gmail.com>
2021-03-19 17:27:38 +0800

    initialise expense route

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 require("dotenv/config");
+app.use(express.json());
+
+const expenseRouter = require("./routes/expense.route");
 
 app.get("/", (req, res) => res.send("starts"));
+app.use("/expense", expenseRouter);
 
 mongoose.connect(
 	process.env.DB_COLLECTION,
